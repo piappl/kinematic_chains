ae9d22b7b64f8c32a69194bf8b209a4f5f9d418e
Jan Fraś <jfras@piap.pl>
2016-01-25 10:41:40 +0100

    moved git root directory to more proper place

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <kinematic_chains/kinematic_chains.hpp>
 #include <cstring>
 
+void help()
+{
+    std::cout<<"Usage:"<<std::endl;
+    std::cout<<"rosrun kinematic_chains kinematic_chains [-arm|-tool relevant_parameter_in_parameter_server [-arm_base|-arm_tip|-tool_base|-tool_tip] relevant_link_name_in_urdf_description]"<<std::endl;
+}
+
 int main(int argc, char **argv)
 {
     for(int i = 1; i< argc; i++)
     {
         if( strcmp(argv[i],"-h") == 0){
-            std::cout<<"Help under construction"<<std::endl;
+            help();
             exit(0);
         }
     }
